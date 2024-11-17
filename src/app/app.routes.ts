0f6f43c3fde3ac1b0122146445554f6ab14abfb4
Yash Bansal <dilboleoberoy@gmail.com>
2024-11-17 14:53:12 +0530

    home setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ChatRoomComponent } from './components/chat-room/chat-room.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'chat', component: ChatRoomComponent},
+];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,MatButtonModule,MatInputModule,MatFormFieldModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+  username: string = '';
+
+  constructor(private router: Router) {}
+
+  joinChat() {
+    sessionStorage.setItem('username', this.username);
+    this.router.navigate(['/chat']);
+  }
 
 }
