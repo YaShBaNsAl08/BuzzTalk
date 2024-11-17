74cf00183d17f4b9b09cd26178c1c8b83bd546ab
Yash Bansal <dilboleoberoy@gmail.com>
2024-11-17 19:06:34 +0530

    userlist, firebase setup

--- src/app/components/chat-room/chat-room.component.ts
+++ src/app/components/chat-room/chat-room.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatServiceService } from '../../services/chat-service.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { UserListComponent } from '../user-list/user-list.component';
 
 @Component({
   selector: 'app-chat-room',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,MatFormFieldModule,MatInputModule,FormsModule,CommonModule,UserListComponent],
   templateUrl: './chat-room.component.html',
   styleUrl: './chat-room.component.css'
 })
-export class ChatRoomComponent {
+export class ChatRoomComponent implements OnInit{
+  messageContent: string = '';
+  messages: Array<{ user: string; text: string }> = [];
+  username: string | null = '';
+
+  constructor(private chatService: ChatServiceService) {}
+  ngOnInit(): void {
+    this.username = sessionStorage.getItem('username') || 'Anonymous';
+
+    // this.chatService.getMessages().subscribe((msgs) => {
+    //   this.messages = msgs;
+    // });
+  }
+  sendMessage() {
+    if (this.username && this.messageContent) {
+      // this.chatService.sendMessage(this.username, this.messageContent);
+      this.messageContent = ''; // Clear input after sending
+    }
+  }
 
 }
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
+  users: string[] = ['Yash','Alice', 'Bob', 'Charlie'];
+
+  constructor(){}
+
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
 
 }
